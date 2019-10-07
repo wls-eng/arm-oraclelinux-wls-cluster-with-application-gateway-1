8089fa34a5c5f18f7d83fc5433bd5659ac53175b
Gurudutt Suryanarayana <gnsuryan@users.noreply.github.com>
2019-10-07 16:38:23 +0530

    Create setupKeyVaultWithCertData.sh

---
+++ setupKeyVaultWithCertData.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function usage()
+{
+  echo "$0 <resourceGroupName> <pathToCertificateFile> <certificatePassword>"
+  ex: $0 test-rg /home/testuser/certificate.pfx Azure123456!
+}
+
+resourceGrpName=$1
+pathToCertFile=$2
+certPassword=$3
+
+if [ "$#" -ne 3 ];
+then
+usage()
+ exit 1
+fi
+
+if [ ! -f "$pathToCertFile" ];
+then
+ echo "Certificate file $pathToCertFile doesn't exist"
+ echo "Please provide a valid certificate file in pfx format"
+ exit 1
+fi
+
+az keyvault create --name "myKeyVault4Cert" --resource-group $resourceGrpName  --location southeastasia
+
+az keyvault update --name "myKeyVault4Cert" --resource-group resourceGrpName --enabled-for-deployment "true"
+
+az keyvault update --name "myKeyVault4Cert" --resource-group resourceGrpName --enabled-for-template-deployment "true"
+
+az keyvault secret set --vault-name myKeyVault4Cert --encoding base64 --description text/plain --name myCertSecretData --file $pathToCertFile
+
+az keyvault secret set --vault-name myKeyVault4Cert --name myCertSecretPassword --value $certPassword
